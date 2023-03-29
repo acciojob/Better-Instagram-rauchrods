d3f03044a90f8ec6389dd44ede49b32033b277ef
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-03-29 21:56:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your code here
+let draggables = document.querySelectorAll("#parent>div");
+
+let container = document.querySelector("#parent");
+
+draggables.forEach((draggable)=>{
+     draggable.addEventListener("dragstart", ()=>{
+       draggable.classList.add("dragging");
+     });
+
+     draggable.addEventListener("dragend", ()=>{
+        draggable.classList.remove("dragging");
+      });
+
+      draggable.addEventListener("dragover", (e)=>{
+        e.preventDefault();
+        draggable.classList.add("selected");
+        //  console.log("dragging")
+      });
+
+      draggable.addEventListener("dragleave", ()=>{
+        draggable.classList.remove("selected");
+        //  console.log("dragging")
+      });
+
+      draggable.addEventListener("drop", ()=>{
+        // draggable.classList.remove("selected");
+        let obj1 = document.querySelector(".dragging");
+        let obj2 = document.querySelector(".selected");
+         obj2 = {...obj1};
+         console.log("dropped")
+      });
+});
